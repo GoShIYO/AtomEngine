75ba2b5dd53dfdcc9e42724417d42b33f67db7b2
GoShIYO <k024g1025@g.neec.ac.jp>
2025-11-20 00:05:50 +0900

    grid修正

--- Engine/Asset/Shaders/GridPS.hlsl
+++ Engine/Asset/Shaders/GridPS.hlsl
@@ -1,10 +1,13 @@
-cbuffer GridCB : register(b0)
+ï»¿cbuffer GridCB : register(b0)
 {
     float4x4 ViewProj;
+
     float3 CameraPos;
     float farClip;
+
     float3 xAxisColor;
     float3 zAxisColor;
+
     float3 majorLineColor;
     float3 minorLineColor;
 };
@@ -20,9 +23,9 @@ float2 fract(float2 x)
     return x - floor(x);
 }
 
-float ComputeDepthFromWorldPos(float3 worldPos)
+float ComputeDepthFromWorldPos(float3 p)
 {
-    float4 clip = mul(float4(worldPos, 1.0), ViewProj);
+    float4 clip = mul(float4(p, 1.0), ViewProj);
     return clip.z / clip.w;
 }
 
@@ -32,6 +35,7 @@ float filteredCheckers(float2 p, float2 dpdx, float2 dpdy)
     float2 w = max(abs(dpdx), abs(dpdy));
     float2 i = 2.0 * (abs(fract((p - 0.5 * w) * 0.5) - 0.5) -
                   abs(fract((p + 0.5 * w) * 0.5) - 0.5)) / w;
+
     return 0.5 - 0.5 * i.x * i.y;
 }
 //Ref:https://iquilezles.org/articles/filterableprocedurals/
@@ -40,9 +44,19 @@ float filteredGrid(float2 p, float2 dpdx, float2 dpdy, float N)
     float2 w = max(abs(dpdx), abs(dpdy));
     float2 a = p + 0.5 * w;
     float2 b = p - 0.5 * w;
+
     float2 i = (floor(a) + min(fract(a) * N, 1.0) -
-              floor(b) - min(fract(b) * N, 1.0)) / (N * w);
+                floor(b) - min(fract(b) * N, 1.0)) / (N * w);
+
     return (1.0 - i.x) * (1.0 - i.y);
+}
+//Ref::blender
+float3 get_axes(float3 pos, float3 fw, float lineSize)
+{
+    float3 d = abs(pos);
+    d /= fw;
+
+    return 1.0 - smoothstep(lineSize, lineSize + 1.0, d);
 }
 
 struct PSOutput
@@ -56,74 +70,66 @@ PSOutput main(VSOutput input)
     float3 ro = CameraPos;
     float3 rd = normalize(input.viewDir);
 
-    float denom = rd.y;
-    if (abs(denom) < 1e-6)
+    if (abs(rd.y) < 1e-6)
         discard;
 
-    float t = -ro.y / denom;
+    float t = -ro.y / rd.y;
     if (t < 0)
         discard;
 
     float3 worldPos = ro + rd * t;
+    float3 P = worldPos;
 
-    //¥á¥¤¥ó¥°¥ê¥Ã¥É
-    const float majorStep = 10.0;
-    
-    float2 uv = worldPos.xz / majorStep;
-    float2 dpdx_uv = ddx(uv);
-    float2 dpdy_uv = ddy(uv);
+Â  Â  // fwidth
+    float3 fw = abs(ddx(P)) + abs(ddy(P));
 
-    float majorMask = filteredGrid(uv, dpdx_uv, dpdy_uv, 500);
-    
-    //¥µ¥Ö¥°¥ê¥Ã¥É
-    float2 minjorUV = worldPos.xz;
-    float2 minjor_dpdx_uv = ddx(minjorUV);
-    float2 minjor_dpdy_uv = ddy(minjorUV);
+Â  Â  //angle fade & alpha
+    float3 viewToGrid = normalize(CameraPos - P);
+    float angle = 1.0 - abs(viewToGrid.y);
+    angle = angle * angle;
+    float fade = 1.0 - angle * angle;
 
-    float minorMask = filteredGrid(minjorUV, minjor_dpdx_uv, minjor_dpdy_uv, 100);
-    
+    float dist = length(CameraPos - P);
+    fade *= saturate(1.0 - dist / (farClip * 0.5f));
+
+Â  Â  //ãƒ¡ã‚¤ãƒ³uv
+    float2 uvMajor = P.xz / 10.0;
+Â  Â  //ã‚µãƒ–ç·šuv
+    float2 uvMinor = P.xz;
+
+    float2 dpdxMajor = ddx(uvMajor);
+    float2 dpdyMajor = ddy(uvMajor);
+
+    float2 dpdxMinor = ddx(uvMinor);
+    float2 dpdyMinor = ddy(uvMinor);
+
+    float majorMask = filteredGrid(uvMajor, dpdxMajor, dpdyMajor, 500);
+    float minorMask = filteredGrid(uvMinor, dpdxMinor, dpdyMinor, 100);
+Â  Â  //ãƒ¡ã‚¤ãƒ³ãƒ©ã‚¤ãƒ³
     float majorLine = saturate(1.0 - majorMask);
+Â  Â  //ã‚µãƒ–ãƒ©ã‚¤ãƒ³
     float minorLine = saturate(1.0 - minorMask);
+Â  Â  
+Â  Â  // xzè½´
+    float3 axes = get_axes(P, fw, 0.5f);
+
+    float axisX = axes.x;
+    float axisZ = axes.z;
     
-    //Ô­µã¤ÎÉ«
-    float3 originColor = float3(1.0, 1.0, 1.0);
+    float axisAlpha = max(axisX, axisZ);
+
+    float3 axisColor = axisX * xAxisColor + axisZ * zAxisColor;
+
     float3 gridColor = lerp(minorLineColor, majorLineColor, majorLine);
 
-    float gridAlpha = max(majorLine, minorLine);
-    
-    //xÝS¤Î¾€
-    float ddx_x = ddx(worldPos.x);
-    float ddy_x = ddy(worldPos.x);
-    float axisWidth_x = max(abs(ddx_x), abs(ddy_x));
-    //zÝS¤Î¾€
-    float ddx_z = ddx(worldPos.z);
-    float ddy_z = ddy(worldPos.z);
-    float axisWidth_z = max(abs(ddx_z), abs(ddy_z));
-
-    const float fade = 0.02;
-    float x_axis_mask = smoothstep(fade + axisWidth_x, fade - axisWidth_x, abs(worldPos.x));
-    float z_axis_mask = smoothstep(fade + axisWidth_z, fade - axisWidth_z, abs(worldPos.z));
-
-    float axisX = saturate(x_axis_mask);
-    float axisZ = saturate(z_axis_mask);
-    
-    float3 axisColor = xAxisColor * axisX + zAxisColor * axisZ;
-    float overlap = axisX * axisZ;
-    axisColor = lerp(axisColor, originColor, overlap);
-
-    float axisAlpha = max(axisX, axisZ);
-    //É«¤òºÏ³É
     float3 color = lerp(gridColor, axisColor, axisAlpha);
-    
-    //¾€¤Î²¿·Ö¤À¤±É«¤ò¸¶¤±¤ë
-    float alpha = max(majorLine, minorLine);
-    alpha = max(alpha, axisAlpha);
 
-    float dist = length(worldPos - CameraPos);
-    alpha *= saturate(1.0 - dist / farClip);
+    float alpha = max(max(majorLine, minorLine), axisAlpha);
+    alpha *= fade;
 
     PSOutput o;
     o.color = float4(color, alpha);
     o.depth = ComputeDepthFromWorldPos(worldPos);
+
     return o;
 }
